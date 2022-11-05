a0088fda47f0625fd739973189e3632cabc92951
zhang <zhang_tao@whu.edu.cn>
2022-11-05 15:22:11 +0800

    add model convert

--- convert_model.py
+++ convert_model.py
@@ -5,6 +5,7 @@ weight = torch.load('./output/model_final.pth')
 model_ = {}
 for key in weight['model'].keys():
     if 'sem_seg_head.pixel_decoder.pixel_decoder.' in key:
+        print(key
         key_ = 'sem_seg_head.pixel_decoder.' + key[len('sem_seg_head.pixel_decoder.pixel_decoder.'):]
     else:
         key_ = key
