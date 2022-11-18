70f1874073b2489993c2f4bd2c23327fa2cec226
zhang tao <zhang_tao@whu.edu.cn>
2022-11-18 10:14:53 +0800

    add contrast

--- minvis/video_maskformer_model.py
+++ minvis/video_maskformer_model.py
@@ -206,7 +206,8 @@ class VideoMaskFormer_frame(nn.Module):
             features = self.backbone(images.tensor)
             outputs = self.sem_seg_head(features)
 
-        outputs['pred_embds'] = self.embed_proj(outputs['pred_embds'].detach().permute(0, 2, 3, 1)).permute(0, 3, 1, 2)
+        # outputs['pred_embds'] = self.embed_proj(outputs['pred_embds'].detach().permute(0, 2, 3, 1)).permute(0, 3, 1, 2)
+        outputs['pred_embds'] = outputs['pred_embds'].detach()
 
         if self.training:
             # mask classification target
