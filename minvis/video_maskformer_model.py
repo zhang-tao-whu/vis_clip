42a0cff0dbff626d88385cbe20c9d039e229600a
zhang tao <zhang_tao@whu.edu.cn>
2022-11-27 11:19:35 +0800

    add mine query tracker

--- minvis/video_maskformer_model.py
+++ minvis/video_maskformer_model.py
@@ -691,14 +691,14 @@ class CrossAttentionLayer_mine(nn.Module):
             if p.dim() > 1:
                 nn.init.xavier_uniform_(p)
 
-    def with_pos_embed(self, tensor, pos: Optional[Tensor]):
+    def with_pos_embed(self, tensor, pos):
         return tensor if pos is None else tensor + pos
 
     def forward_post(self, indentify, tgt, memory,
-                     memory_mask: Optional[Tensor] = None,
-                     memory_key_padding_mask: Optional[Tensor] = None,
-                     pos: Optional[Tensor] = None,
-                     query_pos: Optional[Tensor] = None):
+                     memory_mask=None,
+                     memory_key_padding_mask=None,
+                     pos=None,
+                     query_pos=None):
         tgt2 = self.multihead_attn(query=self.with_pos_embed(tgt, query_pos),
                                    key=self.with_pos_embed(memory, pos),
                                    value=memory, attn_mask=memory_mask,
@@ -709,10 +709,10 @@ class CrossAttentionLayer_mine(nn.Module):
         return tgt
 
     def forward_pre(self, indentify, tgt, memory,
-                    memory_mask: Optional[Tensor] = None,
-                    memory_key_padding_mask: Optional[Tensor] = None,
-                    pos: Optional[Tensor] = None,
-                    query_pos: Optional[Tensor] = None):
+                    memory_mask=None,
+                    memory_key_padding_mask=None,
+                    pos=None,
+                    query_pos=None):
         tgt2 = self.norm(tgt)
         tgt2 = self.multihead_attn(query=self.with_pos_embed(tgt2, query_pos),
                                    key=self.with_pos_embed(memory, pos),
@@ -723,10 +723,10 @@ class CrossAttentionLayer_mine(nn.Module):
         return tgt
 
     def forward(self, indentify, tgt, memory,
-                memory_mask: Optional[Tensor] = None,
-                memory_key_padding_mask: Optional[Tensor] = None,
-                pos: Optional[Tensor] = None,
-                query_pos: Optional[Tensor] = None):
+                memory_mask=None,
+                memory_key_padding_mask=None,
+                pos=None,
+                query_pos=None):
         if self.normalize_before:
             return self.forward_pre(indentify, tgt, memory, memory_mask,
                                     memory_key_padding_mask, pos, query_pos)
