19557034288911a15efd021c6935da8f236370c6
zhang tao <zhang_tao@whu.edu.cn>
2022-12-01 11:56:46 +0800

    maybe fiexd some bugs on critation

--- minvis/video_maskformer_model.py
+++ minvis/video_maskformer_model.py
@@ -427,14 +427,12 @@ class VideoMaskFormer_frame(nn.Module):
         out_logits = torch.stack(out_logits, dim=0) # (t, q, c)
         out_logits_ = out_logits.mean(dim=0) # (q, c)
         is_background = is_background.unsqueeze(2) # (t, q, 1)
-        print(not_whole_background)
         out_logits_[not_whole_background] = ((out_logits * (1 - is_background)).sum(dim=0) /\
                                              ((1 - is_background).sum(dim=0) + 1e-6))[not_whole_background].to(out_logits_.dtype)
         # out_logits = sum(out_logits)/len(out_logits)
         out_logits = out_logits_
         out_masks = torch.stack(out_masks, dim=1)  # q h w -> q t h w
-        #print(is_background)
-        #out_masks = out_masks * is_background.transpose(0, 1).unsqueeze(3)
+        out_masks = out_masks * (1 - is_background).transpose(0, 1).unsqueeze(3) - 1e-6
 
         out_logits = out_logits.unsqueeze(0)
         out_masks = out_masks.unsqueeze(0)
