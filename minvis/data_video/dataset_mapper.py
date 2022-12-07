48cb5ee3a61ed49d86fdd27018939d73f34599da
zhang tao <zhang_tao@whu.edu.cn>
2022-12-07 21:49:48 +0800

    add minvis 720p train config

--- minvis/data_video/dataset_mapper.py
+++ minvis/data_video/dataset_mapper.py
@@ -299,6 +299,7 @@ class YTVISDatasetMapper:
             image = aug_input.image
 
             image_shape = image.shape[:2]  # h, w
+            print(image_shape)
             # Pytorch's dataloader is efficient on torch.Tensor due to shared-memory,
             # but not efficient on large generic data structures due to the use of pickle & mp.Queue.
             # Therefore it's important to use torch.Tensor.
