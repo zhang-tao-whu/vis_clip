24f1dab18ddc385e1a0ea3a9ed85dc3607281a42
zhang tao <zhang_tao@whu.edu.cn>
2022-12-10 17:40:07 +0800

    update ytvis config

--- minvis/video_maskformer_model.py
+++ minvis/video_maskformer_model.py
@@ -138,7 +138,7 @@ class VideoMaskFormer_online(nn.Module):
             cost_mask=mask_weight,
             cost_dice=dice_weight,
             num_points=cfg.MODEL.MASK_FORMER.TRAIN_NUM_POINTS,
-            frames=5
+            frames=cfg.INPUT.SAMPLING_FRAME_NUM
         )
 
         weight_dict = {"loss_ce": class_weight, "loss_mask": mask_weight, "loss_dice": dice_weight,
