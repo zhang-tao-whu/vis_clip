db5fea0c10aa27f5325224fc58b7d5f49ffd78e8
zhang tao <zhang_tao@whu.edu.cn>
2023-02-13 20:35:59 +0800

    update

--- minvis/data_video/vps_eval.py
+++ minvis/data_video/vps_eval.py
@@ -92,9 +92,9 @@ class VPSEvaluator(DatasetEvaluator):
         self._cpu_device = torch.device("cpu")
 
         self._metadata = MetadataCatalog.get(dataset_name)
-        print(self._metadata.keys())
         thing_dataset_id_to_contiguous_id = self._metadata["thing_dataset_id_to_contiguous_id"]
         stuff_dataset_id_to_contiguous_id = self._metadata["stuff_dataset_id_to_contiguous_id"]
+        print(thing_dataset_id_to_contiguous_id)
         self.contiguous_id_to_thing_dataset_id = {}
         self.contiguous_id_to_stuff_dataset_id = {}
         for key in thing_dataset_id_to_contiguous_id:
