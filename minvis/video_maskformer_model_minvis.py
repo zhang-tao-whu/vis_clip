4e6ae56f2db6a7cd2a7a4618adbd209917edc255
zhang tao <zhang_tao@whu.edu.cn>
2023-02-13 18:00:27 +0800

    update

--- minvis/video_maskformer_model_minvis.py
+++ minvis/video_maskformer_model_minvis.py
@@ -423,10 +423,10 @@ class VideoMaskFormer_frame(nn.Module):
             for k in range(cur_classes.shape[0]):
                 pred_class = cur_classes[k].item()
                 isthing = pred_class in self.metadata.thing_dataset_id_to_contiguous_id.values()
+                print(self.metadata.thing_dataset_id_to_contiguous_id.values())
                 mask_area = (cur_mask_ids == k).sum().item()
                 original_area = (cur_masks[k] >= 0.5).sum().item()
                 mask = (cur_mask_ids == k) & (cur_masks[k] >= 0.5)
-                print(pred_class)
                 if mask_area > 0 and original_area > 0 and mask.sum().item() > 0:
                     if mask_area / original_area < self.overlap_threshold:
                         continue
