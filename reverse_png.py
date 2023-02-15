6a152f458fa2578edb425018a5cce8b8d9a93a27
zhang tao <zhang_tao@whu.edu.cn>
2023-02-15 20:07:22 +0800

    add test json gen

---
+++ reverse_png.py
@@ -0,0 +1,19 @@
+import cv2
+import os
+import numpy as np
+
+src = 'output/inference/pan_pred'
+drc = 'output/inference/pan_pred_reverse'
+
+videos = os.listdir(src)
+
+for video in videos:
+    images = os.listdir(os.path.join(src, video))
+    if not os.path.exists(os.path.join(drc, video)):
+        os.makedirs(os.path.join(drc, video))
+    for image in images:
+        seg = np.array(cv2.imread(os.path.join(src, video, image)))
+        seg = seg[:, :, ::-1]
+        cv2.imwrite(os.path.join(drc, video, image), seg)
+
+        
