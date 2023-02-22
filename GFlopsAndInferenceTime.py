a22d0df49e8642a71040a6b7824b53aae33cce61
zhang tao <zhang_tao@whu.edu.cn>
2023-02-22 16:49:57 +0800

    try add GFlops

--- GFlopsAndInferenceTime.py
+++ GFlopsAndInferenceTime.py
@@ -129,11 +129,8 @@ with torch.no_grad():
     instance_embeds = torch.randn(1, 256, 100, 100).to(model.device)
     mask_feature_input = torch.randn(1, 100, 256, 256, input_size[0] // 16, input_size[1] // 16).to(model.device)
     start = time.time()
-    for i in tqdm(range(10)):
-        offline_tracker(instance_embeds, instance_embeds, mask_feature_input)
-        torch.cuda.synchronize()
-    end = time.time()
-    print("offline tracker consumed {} s".format((end - start) / 100. / 10.))
+    offline_tracker(instance_embeds, instance_embeds, mask_feature_input)
+    print("offline tracker consumed {} s".format((end - start) / 100.))
 
 
 
