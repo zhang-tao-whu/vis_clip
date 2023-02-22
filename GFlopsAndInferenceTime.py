a92634a471539dc295697bde27e755d2154e7280
zhang tao <zhang_tao@whu.edu.cn>
2023-02-22 16:15:27 +0800

    try add GFlops

--- GFlopsAndInferenceTime.py
+++ GFlopsAndInferenceTime.py
@@ -119,16 +119,16 @@ with torch.no_grad():
 
     # offline_tracker
     offline_tracker = model.offline_tracker
-    instance_embeds = torch.randn(1, 256, 100, 100).to(model.device)
-    mask_feature_input = torch.randn(1, 100, 256, input_size[0] // 4, input_size[1] // 4).to(model.device)
+    instance_embeds = torch.randn(1, 256, 1, 100).to(model.device)
+    mask_feature_input = torch.randn(1, 1, 256, input_size[0] // 4, input_size[1] // 4).to(model.device)
     flops = FlopCountAnalysis(offline_tracker, (instance_embeds, instance_embeds, mask_feature_input))
     flops.by_module()
     print(flop_count_table(flops))
 
-    instance_embeds = torch.randn(1, 256, 100, 100).to(model.device)
-    mask_feature_input = torch.randn(1, 100, 256, input_size[0] // 4, input_size[1] // 4).to(model.device)
     start = time.time()
-    offline_tracker(instance_embeds, instance_embeds, mask_feature_input)
+    for i in tqdm(range(100)):
+        offline_tracker(instance_embeds, instance_embeds, mask_feature_input)
+        torch.cuda.synchronize()
     end = time.time()
     print("offline tracker consumed {} s".format((end - start) / 100.))
 
