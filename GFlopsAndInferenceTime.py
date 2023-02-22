41441db1f06d22a90e1d9e4d01d25f5ce2f7eb15
zhang tao <zhang_tao@whu.edu.cn>
2023-02-22 16:33:22 +0800

    try add GFlops

--- GFlopsAndInferenceTime.py
+++ GFlopsAndInferenceTime.py
@@ -119,21 +119,21 @@ with torch.no_grad():
 
     # offline_tracker
     offline_tracker = model.offline_tracker
-    instance_embeds = torch.randn(1, 256, 1, 100).to(model.device)
-    mask_feature_input = torch.randn(1, 1, 256, input_size[0] // 4, input_size[1] // 4).to(model.device)
+    instance_embeds = torch.randn(1, 256, 100, 100).to(model.device)
+    mask_feature_input = torch.randn(1, 100, 256, input_size[0] // 4, input_size[1] // 4).to(model.device)
     flops = FlopCountAnalysis(offline_tracker, (instance_embeds, instance_embeds, mask_feature_input))
     flops.by_module()
     print(flop_count_table(flops))
     del instance_embeds, mask_feature_input
 
-    instance_embeds = torch.randn(1, 256, 80, 100).to(model.device)
-    mask_feature_input = torch.randn(1, 80, 256, input_size[0] // 16, input_size[1] // 16).to(model.device)
+    instance_embeds = torch.randn(1, 256, 100, 100).to(model.device)
+    mask_feature_input = torch.randn(1, 100, 256, input_size[0] // 16, input_size[1] // 16).to(model.device)
     start = time.time()
     for i in tqdm(range(10)):
         offline_tracker(instance_embeds, instance_embeds, mask_feature_input)
         torch.cuda.synchronize()
     end = time.time()
-    print("offline tracker consumed {} s".format((end - start) / 80. / 10.))
+    print("offline tracker consumed {} s".format((end - start) / 100. / 10.))
 
 
 
