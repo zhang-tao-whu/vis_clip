27723a0012d7a2a4da9bb29b7e295a58d33ae511
zhang tao <zhang_tao@whu.edu.cn>
2023-03-09 21:47:57 +0800

    try add vss

--- minvis/data_video/datasets/vss.py
+++ minvis/data_video/datasets/vss.py
@@ -184,7 +184,7 @@ def get_metadata():
      {"id": 121, "name": "food", "isthing": 0, "color": [255, 0, 143]},
      {"id": 122, "name": "instrument", "isthing": 1, "color": [0, 255, 235]},
      {"id": 123, "name": "train", "isthing": 1, "color": [133, 255, 0]}]
-    categories_ = {}
+    categories_ = []
     for item in categories:
         if item['isthing']:
             categories_.append(item)
