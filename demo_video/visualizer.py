6f795489b753e72087d7262e56383bf641cc7b64
zhang tao <zhang_tao@whu.edu.cn>
2023-03-09 21:18:14 +0800

    try add vss

--- demo_video/visualizer.py
+++ demo_video/visualizer.py
@@ -83,7 +83,7 @@ class TrackVisualizer(Visualizer):
             dataset_colors = thing_colors + stuff_colors
         if dataset_colors is None:
             dataset_colors = self.metadata.get("colors", None)
-        print(dataset_colors)
+        print(self.metadata)
 
         if ids is None:
             # colors = [
