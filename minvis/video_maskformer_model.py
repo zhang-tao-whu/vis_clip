07929332c32da2593ddc8e10430bfeb0af4f11fc
zhang tao <zhang_tao@whu.edu.cn>
2023-03-16 10:34:39 +0800

    reset inference as mean

--- minvis/video_maskformer_model.py
+++ minvis/video_maskformer_model.py
@@ -473,7 +473,7 @@ class VideoMaskFormer_online(nn.Module):
         scores = F.softmax(out_logits, dim=-1)
         _, labels = scores.max(-1)
         valid_frames = (torch.cumsum((labels != scores.size(-1)).to(torch.float32), dim=0) != 0).to(torch.float32)
-        out_logits = (out_logits * valid_frames).sum(dim=0) / valid_frames.sum(dim=0)
+        out_logits = (valid_frames * out_logits).sum(dim=0) / valid_frames.sum(dim=0)
 
         out_masks = torch.stack(out_masks, dim=1)  # q h w -> q t h w
 
