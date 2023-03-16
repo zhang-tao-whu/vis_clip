2d067e6af11d8496982e16a890358dba89a16e76
zhang tao <zhang_tao@whu.edu.cn>
2023-03-16 22:06:22 +0800

    try online use segmenter norm, class embed, mask embed, and frozen them

--- minvis/video_maskformer_model.py
+++ minvis/video_maskformer_model.py
@@ -1081,11 +1081,11 @@ class QueryTracker_mine(torch.nn.Module):
                         # )
                         ms_output.append(output)
             else:
+                indices = self.match_embds(self.decoder_norm(self.last_frame_embeds),
+                                           self.decoder_norm(single_frame_embeds))
+                print(indices)
+                self.last_frame_embeds = single_frame_embeds[indices]
                 for j in range(self.num_layers):
-                    indices = self.match_embds(self.decoder_norm(self.last_frame_embeds),
-                                               self.decoder_norm(single_frame_embeds))
-                    print(indices)
-                    self.last_frame_embeds = single_frame_embeds[indices]
                     if j == 0:
                         ms_output.append(single_frame_embeds[indices])
                         # indices = self.match_embds(self.last_frame_embeds, single_frame_embeds)
