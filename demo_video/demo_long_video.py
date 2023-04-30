8fd0cb38bf9cc788b744ab08fa06ab6208ea15d2
zhang tao <zhang_tao@whu.edu.cn>
2023-04-30 15:25:07 +0800

    first commit

--- demo_video/demo_long_video.py
+++ demo_video/demo_long_video.py
@@ -28,7 +28,7 @@ from detectron2.utils.logger import setup_logger
 
 from mask2former import add_maskformer2_config
 from mask2former_video import add_maskformer2_video_config
-from minvis import add_minvis_config
+from dvis import add_minvis_config, add_dvis_config
 from predictor import VisualizationDemo, VisualizationDemo_windows
 
 import shutil
@@ -41,6 +41,7 @@ def setup_cfg(args):
 	add_maskformer2_config(cfg)
 	add_maskformer2_video_config(cfg)
 	add_minvis_config(cfg)
+	add_dvis_config(cfg)
 	cfg.merge_from_file(args.config_file)
 	cfg.merge_from_list(args.opts)
 	cfg.freeze()
