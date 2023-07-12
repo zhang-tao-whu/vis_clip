833f8ab9cd37a6331079ab31b72c472bb3b6a3a3
zhang tao <zhang_tao@whu.edu.cn>
2023-07-12 22:59:20 +0800

    add vit adapter config

--- mask2former/modeling/__init__.py
+++ mask2former/modeling/__init__.py
@@ -1,5 +1,6 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
 from .backbone.swin import D2SwinTransformer
+from .backbones_vitAdapter.adapter import D2VitAdapterDinoV2
 from .pixel_decoder.fpn import BasePixelDecoder
 from .pixel_decoder.msdeformattn import MSDeformAttnPixelDecoder
 from .meta_arch.mask_former_head import MaskFormerHead
