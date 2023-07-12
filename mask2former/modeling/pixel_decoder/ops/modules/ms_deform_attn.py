c678f5713130be999579c0ad030a86b5d459fc6e
zhang tao <zhang_tao@whu.edu.cn>
2023-07-12 23:04:12 +0800

    add vit adapter config

--- mask2former/modeling/pixel_decoder/ops/modules/ms_deform_attn.py
+++ mask2former/modeling/pixel_decoder/ops/modules/ms_deform_attn.py
@@ -32,7 +32,7 @@ def _is_power_of_2(n):
 
 
 class MSDeformAttn(nn.Module):
-    def __init__(self, d_model=256, n_levels=4, n_heads=8, n_points=4):
+    def __init__(self, d_model=256, n_levels=4, n_heads=8, n_points=4, ratio=1.0):
         """
         Multi-Scale Deformable Attention Module
         :param d_model      hidden dimension
