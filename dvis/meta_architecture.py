b3c7a70302a38574e8e2a9f35bd5bd84a79a8dda
zhang tao <zhang_tao@whu.edu.cn>
2023-07-13 16:19:19 +0800

    change, try set a stable version with noise train

--- dvis/meta_architecture.py
+++ dvis/meta_architecture.py
@@ -1235,7 +1235,7 @@ class DVIS_offline(DVIS_online):
             # mask classification target
             targets = self.prepare_targets(batched_inputs, images)
             # use the online prediction results to guide the matching process during early training phase
-            if self.iter < self.max_iter_num // 2:
+            if self.iter < min(self.max_iter_num // 2, 2000):
                 image_outputs, outputs, targets = self.frame_decoder_loss_reshape(
                     outputs, targets, image_outputs=image_outputs
                 )
