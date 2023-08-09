81fffeb1072c5b330342edf3ac16e8fa64ab452e
zhang tao <zhang_tao@whu.edu.cn>
2023-08-09 20:23:08 +0800

    only cl 0-th gt

--- dvis/ClTracker.py
+++ dvis/ClTracker.py
@@ -1134,6 +1134,8 @@ class ClDVIS_online(MinVIS):
             for i_gt in gt2ref.keys():
                 # if gt_ids[i_gt] == -1 or gt_ids_[i_gt] == -1:
                 #     continue
+                if gt_ids[i_gt] == -1 or gt_ids_[i_gt]:
+                    continue
                 i_ref = gt2ref[i_gt]
                 i_key = gt2key[i_gt]
 
