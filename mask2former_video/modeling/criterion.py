7740f9feb70039003e226ba63c5c5af796480383
zhang tao <zhang_tao@whu.edu.cn>
2023-08-12 14:12:56 +0800

    try ov

--- mask2former_video/modeling/criterion.py
+++ mask2former_video/modeling/criterion.py
@@ -149,10 +149,7 @@ class VideoSetCriterion(nn.Module):
         assert "pred_masks" in outputs
 
         src_idx = self._get_src_permutation_idx(indices)
-        print(src_idx)
         src_masks = outputs["pred_masks"]
-        print(src_masks.shape)
-        return {}
         src_masks = src_masks[src_idx]
         # Modified to handle video
         target_masks = torch.cat([t['masks'][i] for t, (_, i) in zip(targets, indices)]).to(src_masks)
