a82994dfd260491fb0a12ca73d3075565ab9fb08
zhang tao <zhang_tao@whu.edu.cn>
2023-08-13 15:38:40 +0800

    miinvis work

--- dvis/meta_architecture_ov.py
+++ dvis/meta_architecture_ov.py
@@ -236,6 +236,7 @@ class MinVIS_OV(nn.Module):
             return self.test_text_classifier, self.test_num_templates
 
     def set_metadata(self, name, metadata):
+        print(metadata['thing_classes'])
         self.category_overlapping_mask, self.test_num_templates, self.test_class_names = \
             self.prepare_class_names_from_metadata(metadata, self.all_train_metadatas)
         self.test_class_prepares.update({name: {'overlapping': self.category_overlapping_mask,
