18fa94f65687b642f66079920f3e1c673d639acb
zhang tao <zhang_tao@whu.edu.cn>
2023-08-14 11:38:58 +0800

    update dvis_tracker

--- demo_video/predictor.py
+++ demo_video/predictor.py
@@ -226,7 +226,8 @@ class VideoPredictor(DefaultPredictor):
         weight = torch.load(cfg.MODEL.WEIGHTS)
         if 'model' in weight.keys():
             weight = weight['model']
-        self.model.load_state_dict(weight, strict=True)
+        # self.model.load_state_dict(weight, strict=True)
+        self.model.load_state_dict(weight, strict=False)
 
         self.aug = T.ResizeShortestEdge(
             [cfg.INPUT.MIN_SIZE_TEST, cfg.INPUT.MIN_SIZE_TEST], cfg.INPUT.MAX_SIZE_TEST
