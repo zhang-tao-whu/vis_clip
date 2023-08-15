1777c278ccc23ccd4099b28976c7faf7264fd4d1
zhang tao <zhang_tao@whu.edu.cn>
2023-08-15 16:51:05 +0800

    multi void embeds

--- dvis/meta_architecture_ov.py
+++ dvis/meta_architecture_ov.py
@@ -51,10 +51,11 @@ def get_classification_logits(x, text_classifier, logit_scale, num_templates=Non
     # max ensembel as in OpenSeg/ODISE
     final_pred_logits = []
     cur_idx = 0
-    for num_t in num_templates:
+    for num_t in num_templates[:-1]:
         final_pred_logits.append(pred_logits[:, :, cur_idx: cur_idx + num_t].max(-1).values)
         cur_idx += num_t
-    final_pred_logits.append(pred_logits[:, :, -1]) # the last classifier is for void
+    # final_pred_logits.append(pred_logits[:, :, -1]) # the last classifier is for void
+    final_pred_logits.append(pred_logits[:, :, -num_templates[-1]:].min(-1).values)
     final_pred_logits = torch.stack(final_pred_logits, dim=-1)
     return final_pred_logits
 
