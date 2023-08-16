eaba439fefaa7f0fe3ae0f7fb912cf2016a29b36
zhang tao <zhang_tao@whu.edu.cn>
2023-08-16 16:52:57 +0800

    try debug

--- dvis/meta_architecture_ov.py
+++ dvis/meta_architecture_ov.py
@@ -493,7 +493,6 @@ class MinVIS_OV(nn.Module):
         name = batched_inputs[0]['name']
         for batched_input in batched_inputs:
             assert name == batched_input['name']
-        print(len(batched_inputs), name)
         images = []
         for video in batched_inputs:
             for frame in video["image"]:
