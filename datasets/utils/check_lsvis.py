f39e6df729bf8c18ef77c4f96cbbc0de7ca027d6
zhang tao <zhang_tao@whu.edu.cn>
2023-08-30 16:31:05 +0800

    add check lvvis

---
+++ datasets/utils/check_lsvis.py
@@ -0,0 +1,25 @@
+import json
+import os
+from PIL import Image
+
+json_file = '../lsvis/train_instances.json'
+with open(json_file, 'r') as f:
+    json_file = json.load(f)
+
+videoID2videos = {}
+for item in json_file['videos']:
+    id = item["id"]
+    videoID2videos[id] = item
+
+for anno in json_file['annotations']:
+    video_id = anno['video_id']
+    height, width = anno['height', 'width']
+    video_info = videoID2videos[video_id]
+    video_height, video_width = video_info['height'], video_info['width']
+    images_files = video_info["file_names"]
+    images_files = [os.path.join('../lsvis/train/JPEGImages', item) for item in images_files]
+    assert video_height == height and video_width == width, print((height, width), (video_height, video_width))
+    for file in images_files:
+        image = Image.open(file)
+        image_width, image_height = image.size
+        assert video_height == image_height and video_width == image_width, print((height, width), (image_height, image_width))
