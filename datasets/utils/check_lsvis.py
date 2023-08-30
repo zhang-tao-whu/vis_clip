6a67d22f1b8a1035a6748848f3e12d6bb66f2139
zhang tao <zhang_tao@whu.edu.cn>
2023-08-30 17:24:51 +0800

    add check lvvis

--- datasets/utils/check_lsvis.py
+++ datasets/utils/check_lsvis.py
@@ -11,6 +11,9 @@ for item in json_file['videos']:
     id = item["id"]
     videoID2videos[id] = item
 
+print(json_file.keys())
+
+wrong_videos_id = []
 for anno in json_file['annotations']:
     video_id = anno['video_id']
     height, width = anno['height'], anno['width']
@@ -23,10 +26,32 @@ for anno in json_file['annotations']:
         image = Image.open(file)
         image_width, image_height = image.size
         if not (video_height == image_height and video_width == image_width):
-            if anno['segmentations'][i] is None:
-                print(i, file, video_info)
-                print(anno['segmentations'][:i])
-            else:
-                if not isinstance(anno['segmentations'][i], dict):
-                    print(anno['segmentations'][i], type(anno['segmentations'][i]))
-                print(file, (height, width), (image_height, image_width), anno['segmentations'][i]["size"])
+            wrong_videos_id.append(video_id)
+        if anno['segmentations'][i] is None:
+            wrong_videos_id.append(video_id)
+            # if anno['segmentations'][i] is None:
+            #     print(i, file, video_info)
+            #     print(anno['segmentations'][:i])
+            # else:
+            #     if not isinstance(anno['segmentations'][i], dict):
+            #         print(anno['segmentations'][i], type(anno['segmentations'][i]))
+            #     print(file, (height, width), (image_height, image_width), anno['segmentations'][i]["size"])
+
+wrong_videos_id = set(wrong_videos_id)
+
+videos_ = []
+for video in json_file["videos"]:
+    if video["id"] in wrong_videos_id:
+        pass
+    else:
+        videos_.append(video)
+print(len(videos_), '/', len(json_file["videos"]))
+json_file["videos"] = videos_
+
+annotations_ = []
+for anno in json_file['annotations']:
+    video_id = anno['video_id']
+    if video_id in wrong_videos_id:
+        annotations_.append(anno)
+print(len(annotations_), '/', len(json_file["annotations"]))
+json_file["annotations"] = annotations_
