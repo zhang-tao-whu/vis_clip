1f08e43da51d0a396c8c9593c8d198be9a6e3fdf
zhang tao <zhang_tao@whu.edu.cn>
2023-10-15 16:45:52 +0800

    update vspw inference

--- mask2former/modeling/matcher.py
+++ mask2former/modeling/matcher.py
@@ -108,8 +108,10 @@ class HungarianMatcher(nn.Module):
             # Compute the classification cost. Contrary to the loss, we don't use the NLL,
             # but approximate it in 1 - proba[target class].
             # The 1 is a constant that doesn't change the matching, it can be ommitted.
-            cost_class = -out_prob[:, tgt_ids]
-
+            try:
+                cost_class = -out_prob[:, tgt_ids]
+            except:
+                cost_class = 0.0
             out_mask = outputs["pred_masks"][b]  # [num_queries, H_pred, W_pred]
             # gt masks are already padded when preparing target
             tgt_mask = targets[b]["masks"].to(out_mask)
