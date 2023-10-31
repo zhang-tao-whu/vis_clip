aba468d7ec34ccd0c513e8d71e726d75f4998e55
zhang tao <zhang_tao@whu.edu.cn>
2023-10-31 17:48:24 +0800

    explore combine denosing and propogation

--- dvis/utils.py
+++ dvis/utils.py
@@ -36,7 +36,6 @@ class Noiser:
         return indices, noise_init
 
     def _wa_noise_forward(self, cur_embeds, cur_classes):
-        assert cur_classes is not None
         # embeds (q, b, c), classes (q)
         indices = list(range(cur_embeds.shape[0]))
         np.random.shuffle(indices)
