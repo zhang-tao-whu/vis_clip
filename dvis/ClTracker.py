04eb1010dfe2130308af173692934f1158b8dc76
zhang tao <zhang_tao@whu.edu.cn>
2023-11-24 23:12:27 +0800

    try filter bg

--- dvis/ClTracker.py
+++ dvis/ClTracker.py
@@ -329,6 +329,8 @@ class ClReferringTracker_noiser(torch.nn.Module):
 
         if self.filer_bg and single_frame_classes is not None:
             attn_mask = single_frame_classes == -1
+            if attn_mask.to(torch.float32).sum() == attn_mask.shape[0]:
+                attn_mask[:] = False
             attn_mask = attn_mask.unsqueeze(0).repeat(frame_embeds_no_norm.shape[0], 1)  # (q, q)
             attn_mask = attn_mask.to(frame_embeds_no_norm.device)
         else:
