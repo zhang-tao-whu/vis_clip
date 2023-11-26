f88d80bbb3c12225c17af58f0d36378a7eda3d8b
zhang tao <zhang_tao@whu.edu.cn>
2023-11-26 14:10:52 +0800

    memory works, 4w iter 38.0

--- dvis/ClTracker.py
+++ dvis/ClTracker.py
@@ -334,6 +334,7 @@ class ClReferringTracker_noiser(torch.nn.Module):
         return output
 
     def _use_memories(self, references):
+        
         self.memories.append(references)
         if len(self.memories) > self.memories_max_length:
             self.memories = self.memories[-self.memories_max_length:]
