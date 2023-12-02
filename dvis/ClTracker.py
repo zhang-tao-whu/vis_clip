52f8afbcf7526e82ef8286ef7d9d188393ccd590
zhang tao <zhang_tao@whu.edu.cn>
2023-12-02 17:03:50 +0800

    try reset gt class labels

--- dvis/ClTracker.py
+++ dvis/ClTracker.py
@@ -940,8 +940,10 @@ class ClDVIS_online(MinVIS):
         #out_logits = torch.mean(pred_logits, dim=0).unsqueeze(0)
 
         # try new score compute
+        scores = F.softmax(pred_logits, dim=-1)[:, :, :-1]
+        scores = torch.max(scores, dim=-1)[0]
         max_scores = torch.max(pred_logits.softmax(dim=-1)[..., :-1], dim=-1)[0]
-        print(torch.max(max_scores, dim=0))
+        print(torch.max(max_scores, dim=0), torch.max(scores, dim=0))
         cummax_scores = torch.cummax(max_scores, dim=0)[0]
         valid = cummax_scores > 0.1
         valid_nums = torch.sum(valid.to(torch.float32), dim=0)  # (q)
